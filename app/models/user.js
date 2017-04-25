2adfed50057907281aae2b01f50836ca36b4b3bc
Krzysiek Antecki <akrisu@gmail.com>
2017-04-25 11:59:25 +0200

    empty files for user model and routes

---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+
+});
+
+UserSchema.pre('save', next => {
+    now = new Date();
+
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('user', UserSchema);
